b8b5a31fd5f02a845e2584d444136fb38f14bda9
Rachel Joyce <rjoyce@splunk.com>
2025-08-16 13:29:23 -0600

    Fix test path

--- src/test/java/org/example/simplehome/HomeControllerTest.java
+++ src/test/java/org/example/simplehome/HomeControllerTest.java
@@ -1,9 +1,10 @@
-package org.example;
+package org.example.simplehome;
 
 import org.example.simplehome.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 
